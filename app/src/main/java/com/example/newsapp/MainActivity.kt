2dab2c91e90c58b0df8debc34eab9fbc94edad0a
Techsultan <salamsultan09@gmail.com>
2023-01-06 12:41:37 +0100

    Change the Apps theme and added a drawer layout to the toolbar

--- app/src/main/java/com/example/newsapp/MainActivity.kt
+++ app/src/main/java/com/example/newsapp/MainActivity.kt
@@ -2,10 +2,12 @@ package com.example.newsapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.drawerlayout.widget.DrawerLayout
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupWithNavController
 import com.example.newsapp.databinding.ActivityMainBinding
 import com.example.newsapp.db.ArticleDatabase
@@ -13,6 +15,7 @@ import com.example.newsapp.repository.NewsRepository
 import com.example.newsapp.ui.NewsViewModel
 import com.example.newsapp.ui.NewsViewModelProviderFactory
 import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.google.android.material.navigation.NavigationView
 
 
 class MainActivity : AppCompatActivity() {
@@ -29,12 +32,20 @@ class MainActivity : AppCompatActivity() {
         val viewModelProviderFactory = NewsViewModelProviderFactory(newsRepository)
         viewModel = ViewModelProvider(this, viewModelProviderFactory)[NewsViewModel::class.java]
 
-        val host: NavHostFragment = supportFragmentManager
-            .findFragmentById(R.id.newsNavHostFragment) as NavHostFragment? ?: return
+        val navHostFragment = supportFragmentManager
+            .findFragmentById(R.id.newsNavHostFragment) as NavHostFragment
 
-        val navController = host.navController
+        val navController = navHostFragment.navController
 
+        val drawerLayout: DrawerLayout = binding.drawerLayout
+        val appBarConfiguration = AppBarConfiguration(
+            setOf(R.id.breakingNewsFragment, R.id.savedNewsFragment, R.id.searchNewsFragment), drawerLayout)
+
+        binding.toolbar.setupWithNavController(navController, appBarConfiguration)
         setupBottomNavMenu(navController)
+        setupDrawerLayout(navController)
+
+
 
     }
 
@@ -42,5 +53,10 @@ class MainActivity : AppCompatActivity() {
 
         val bottomNav = findViewById<BottomNavigationView>(R.id.bottomNavigationView)
         bottomNav?.setupWithNavController(navController)
+    }
+
+    private fun setupDrawerLayout(navController: NavController) {
+        val drawerLayout = findViewById<NavigationView>(R.id.nav_view)
+        drawerLayout.setupWithNavController(navController)
     }
 }
